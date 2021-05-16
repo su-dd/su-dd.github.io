a2857a9901837a228c1e28918e2349a28e84d93e
sul <987380454@qq.com>
2021-05-17 00:50:25 +0800

    Updates

--- src/containers/TagPost/actionTypes.js
+++
@@ -1,3 +0,0 @@
-export const GET_POST_INFO = 'get_post_info';
-export const SET_TAG_NAME = 'set_tag_name';
-export const FETCH_TAG_POST_LIST = 'fetch_tag_post_list';
--- src/utils/index.js
+++
@@ -1,8 +0,0 @@
-export const randomId = (size = 8) => {
-  const str = 'plokmijnuhbygvtfcrdxeszwaq1234567890';
-  let result = '';
-  while (result.length < size) {
-    result += str.charAt(Math.floor(str.length * Math.random()));
-  }
-  return result;
-};
